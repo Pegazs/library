99b5255cecfe4a7cb76e51bf9956e890c08c5719
yakma <yakmanyakman@yandex.ru>
2018-03-23 18:07:44 +0300

    fixed untracked files

--- html/dbconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-//connect to mysql database
-$con = mysqli_connect("localhost", "LOGIN", "PASSWORD", "testingDB") or die("Error " . mysqli_error($con));
-mysqli_query($con , "SET NAMES utf8");
-?>
--- html/library/dbconnectLib.php
+++
@@ -1,5 +0,0 @@
-<?php
-//connect to mysql database
-$conLib = mysqli_connect("localhost", "LOGIN", "PASSWORD", "testingDB") or die("Error " . mysqli_error($con));
-mysqli_query($conLib , "SET NAMES utf8");
-?>
